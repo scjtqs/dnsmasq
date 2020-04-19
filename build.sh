b33399d0e5dcb1de61d85115e99ad25cefb82fe3
scjtqs <scjtqs@qq.com>
2020-04-19 14:43:37 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
-#docker buildx build --tag scjtqs/dnsmasq:latest --platform linux/amd64,linux/arm64,linux/386,linux/armhf --push .
-docker buildx build --tag scjtqs/dnsmasq:latest --platform linux/amd64 --push .
+docker buildx build --tag scjtqs/dnsmasq:latest --platform linux/amd64,linux/arm64,linux/386,linux/armhf --push .
 docker buildx rm  mybuilder
 
